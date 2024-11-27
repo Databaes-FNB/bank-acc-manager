3e050421cf1d062e7c71c962e48e4b6540525d60
Tumisang9x <163140949+Tumisang9x@users.noreply.github.com>
2024-11-27 13:11:33 +0200

    Main account added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,47 @@
-public class Main{
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        System.out.println("Please enter the initial amount");
+        long initialBalance = sc.nextLong();
+
+        ChequeAccount chqAcc = new ChequeAccount((int) initialBalance);
+
+       do {
+            System.out.println("\n------- Cheque Account Menu ------");
+            System.out.println("1.  Deposit");
+            System.out.println("2. Withdrawal");
+            System.out.println("3  Exit");
+            System.out.println("Please enter your choice:  ");
+            choice = sc.nextInt();
+
+            switch (choice){
+
+                case 1:
+                    chqAcc.deposit();
+                    break;
+
+                case 2:
+                    chqAcc.withdrawal();
+                    break;
+
+                case 3:
+                    System.out.println("Exiting the program.");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+
+            }
+
+        } while (choice != 3);
+       sc.close();
 
 
 
-
+    }
 }
